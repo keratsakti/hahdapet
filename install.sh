3b87d27149a40779b81eaf72fdf5c9c5df0784ec
keratsakti <86237834+keratsakti@users.noreply.github.com>
2021-06-24 22:52:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-git clone https://github.com/keratsakti/Nihil.gits && cd Nihil
+git clone https://github.com/keratsakti/Nihil.git && cd Nihil
 chmod +x yes.sh && chmod +x docker
 sudo adduser --disabled-password --gecos "" hah && sudo usermod -aG sudo hah
 sudo -u hah -H sh -c "./yes.sh"
